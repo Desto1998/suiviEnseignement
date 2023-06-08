8a835c1a87b4c0bd1843ad4738735d21c7adde02
Desto Tambu <teneyemdesto@gmail.com>
2023-06-08 20:52:10 +0100

    commit auth and jwt

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,93 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
 {
     //
+
+    public function __construct()
+    {
+        $this->middleware('auth:api');
+    }
+
+    public function index()
+    {
+        $todos = User::all();
+        return response()->json([
+            'status' => 'success',
+            'todos' => $todos,
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'description' => 'required|string|max:255',
+        ]);
+
+        $todo = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => $request->password,
+            'matricule' => $request->matricule,
+            'tel' => $request->tel,
+        ]);
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Todo created successfully',
+            'todo' => $todo,
+        ]);
+    }
+
+    public function show($id)
+    {
+        $todo = User::find($id);
+        return response()->json([
+            'status' => 'success',
+            'todo' => $todo,
+        ]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|max:255',
+            'password' => 'required|string|max:255',
+            'matricule' => 'required|string|max:255',
+            'tel' => 'required|string|max:255',
+        ]);
+
+        $todo = User::find($id);
+        $todo->name = $request->name;
+        $todo->email = $request->email;
+        $todo->password = Hash::make($request->password);
+        $todo->matricule = $request->matricule;
+        $todo->tel = $request->tel;
+        $todo->save();
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Todo updated successfully',
+            'todo' => $todo,
+        ]);
+    }
+
+    public function destroy($id)
+    {
+        $todo = User::find($id);
+        $todo->delete();
+
+        return response()->json([
+            'status' => 'success',
+            'message' => 'Todo deleted successfully',
+            'todo' => $todo,
+        ]);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +18,20 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::controller(AuthController::class)->group(function () {
+    Route::post('login', 'login');
+    Route::post('register', 'register');
+    Route::post('logout', 'logout');
+    Route::post('refresh', 'refresh');
+
+});
+
+Route::controller(UserController::class)->group(function () {
+    Route::get('todos', 'index');
+    Route::post('todo', 'store');
+    Route::get('todo/{id}', 'show');
+    Route::put('todo/{id}', 'update');
+    Route::delete('todo/{id}', 'destroy');
 });
